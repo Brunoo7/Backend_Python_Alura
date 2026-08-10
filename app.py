e8dc2a93536735e40c7924245719bc7a4c033fbc
Brunoo7 <brunoolivera072@gmail.com>
2026-08-10 09:33:14 -0300

    reorganização da exibição do menu e da lógica de seleção de opções por meio de funções

--- app.py
+++ app.py
@@ -1,40 +1,48 @@
 import os
-print("=====================================================================")
-print("""
+
+def finalizar_app():
+    print("Fechando o app...")
+    os.system("cls")
+
+def exibir_opcoes():
+    print("1. Cadastrar restautante")
+    print("2. Listar restaurantes")
+    print("3. Ativar restaurante")
+    print("4. Sair")
+
+def escolher_opcao():
+
+    opcao = int(input(print("Escolha uma opção:")))
+    match opcao:
+
+        case 1:
+            print("1. Cadastrar restautante")
+        
+        case 2:
+            print("2. Listar restaurantes")
+        
+        case 3:
+            print("3. Ativar restaurante")
+
+        case 4:
+            finalizar_app()
+        
+        case _:
+            print("Opção inválida")
+
+def exibir_nome_do_programa():
+    print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
 ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░\n""")
-print("1. Cadastrar restautante")
-print("2. Listar restaurantes")
-print("3. Ativar restaurante")
-print("4. Sair")
-opcao = int(input(print("Escolha uma opção:")))
-print("=====================================================================")
 
-def finalizar_app():
-    print("Fechando o app...")
-    os.system("cls")
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
 
-
-if opcao == 1: {
-    print("1. Cadastrar restautante")
-}
-
-elif opcao == 2: {
-    print("2. Listar restaurantes")
-}
-
-elif opcao == 3: {
-    print("3. Ativar restaurante")
-}
-
-elif opcao == 4: {
-    finalizar_app()
-}
-
-else:
-    print("Opção inválida")
-    
+if __name__ == '__main__':
+    main()
