6380d056582c0c712663fdc8be0fd747180d2a84
Brunoo7 <brunoolivera072@gmail.com>
2026-08-11 16:22:56 -0300

    melhoramento no tratamento de opções e  feedback para opções inválidas

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ def finalizar_app():
     print("Fechando o app...")
     os.system("cls")
 
+def opcao_invalida():
+    print("Opção inválida!")
+    input("Selecione uma tecla para voltar ao menu principal: ")
+    main()
+
 def exibir_opcoes():
     print("1. Cadastrar restautante")
     print("2. Listar restaurantes")
@@ -11,24 +16,25 @@ def exibir_opcoes():
     print("4. Sair")
 
 def escolher_opcao():
+    try:
+        opcao = int(input(print("Escolha uma opção:")))
+        match opcao:
+            case 1:
+                print("1. Cadastrar restautante")
+            
+            case 2:
+                print("2. Listar restaurantes")
+            
+            case 3:
+                print("3. Ativar restaurante")
 
-    opcao = int(input(print("Escolha uma opção:")))
-    match opcao:
-
-        case 1:
-            print("1. Cadastrar restautante")
-        
-        case 2:
-            print("2. Listar restaurantes")
-        
-        case 3:
-            print("3. Ativar restaurante")
-
-        case 4:
-            finalizar_app()
-        
-        case _:
-            print("Opção inválida")
+            case 4:
+                finalizar_app()
+            
+            case _:
+                print("Opção inválida")
+    except:
+        opcao_invalida
 
 def exibir_nome_do_programa():
     print("""
@@ -40,6 +46,7 @@ def exibir_nome_do_programa():
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░\n""")
 
 def main():
+    os.system("cls")
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
